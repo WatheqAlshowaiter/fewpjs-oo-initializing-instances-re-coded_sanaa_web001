1318238d8955da71d795987ebcecdf1e2e659468
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-28 22:10:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code here
 // 1. Create a class for `Breakfast`. `Breakfast` will have a constructor with a food and a drink.
 class Breakfast {
-  food;
-  drink;
+//   food;
+//   drink;
   constructor(food, drink) {
     this.food = food;
     this.drink = drink;
@@ -11,9 +11,9 @@ class Breakfast {
 
 // 2. Create a class for `Lunch`. `Lunch` will have a constructor with a salad, a soup, and a drink.
 class Lunch {
-  salad;
-  soup;
-  drink;
+//   salad;
+//   soup;/
+//   drink;
   constructor(salad, soup, drink) {
     this.salad = salad;
     this.soup = soup;
@@ -26,10 +26,10 @@ class Lunch {
 //   Indicate that dessert is intended to be a private property by including
 //   an underscore (`_`) at the beginning.
 class Dinner {
-  salad;
-  soup;
-  entree;
-  _dessert;
+//   salad;
+//   soup;
+//   entree;
+//   _dessert;
   constructor(salad, soup, entree, _dessert) {
     this.salad = salad;
     this.soup = soup;
